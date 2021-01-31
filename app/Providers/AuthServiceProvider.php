0b5288c3e380e39aba6a901f415bdd215e3cae42
rofiimdad <64058585+rofiimdad@users.noreply.github.com>
2021-01-31 20:44:21 +0700

    Revert "Merge branch 'main' of https://github.com/tjeret/inventori into main"
    This reverts commit 247a4d452fac9e8b7f90daed1b136e0fb68f3f93, reversing
    changes made to a83656300b24f72401f163373e245a34338126e1.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,13 +18,13 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
-     * Passport
+     *
      * @return void
      */
     public function boot()
     {
         $this->registerPolicies();
 
-        Passport::routes();
+        //
     }
 }
