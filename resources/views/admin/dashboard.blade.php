fbf97323a068306d6713503ce12e63623044f851
rofiimdad <64058585+rofiimdad@users.noreply.github.com>
2021-02-01 23:59:16 +0700

    Add Profile View
    Profile View

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>\
+  <title>
       @yield('title')
   </title>
   <!-- Tell the browser to be responsive to screen width -->
